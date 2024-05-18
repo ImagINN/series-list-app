cda62a8ad56754829fd44651c5bb0bb1e4204801
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-05-19 00:49:03 +0300

    chore: Update main.dart with color scheme and localization support

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:series_list_app/my_series.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
+// ignore: must_be_immutable
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.lightGreen);
+  var kDarkColorScheme = ColorScheme.fromSeed(
+      seedColor: const Color.fromARGB(255, 42, 54, 9),
+      brightness: Brightness.dark);
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MySeries(
-        title: 'My Series',
-      )
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('tr'),
+          Locale('en'),
+        ],
+        darkTheme: ThemeData.dark().copyWith(colorScheme: kDarkColorScheme),
+        theme: ThemeData().copyWith(
+            colorScheme: kColorScheme,
+            appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer,
+            ),
+            cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+            ),
+            elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            )),
+            textTheme: ThemeData().textTheme.copyWith(
+                  titleLarge:
+                      TextStyle(color: kColorScheme.onSecondaryContainer),
+                  titleMedium: TextStyle(
+                      color: kColorScheme.onTertiaryContainer, fontSize: 18),
+                )),
+        themeMode: ThemeMode.system,
+        home: const MySeries(
+          title: 'My Series',
+        ));
   }
 }
